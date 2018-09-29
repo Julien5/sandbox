3bea7fe5ca8b332677df10ccfe197c5dba34d17e
Julien5 <julien_bourgeois@yahoo.fr>
2018-09-29 14:03:34 +0200

    version with interrups, no sleep

--- hamster-rad/main.ino
+++ hamster-rad/main.ino
@@ -3,20 +3,25 @@
 const int led_pin = 4;
 const int reed_pin = 2;
 
-int last_time_change=0;
+int n=0;
+
+void on_rising_reed() {
+	n++;
+}
 
 void setup() {
   for(int k=0; k<10; ++k)
 	pinMode(led_pin+k, OUTPUT);
-  pinMode(reed_pin, INPUT);
+  pinMode(reed_pin, INPUT_PULLUP);
+  attachInterrupt(0,on_rising_reed,RISING);
   Serial.begin(9600);
 }
 
 void sleepNow() {
+	return;
 	Serial.println("going to sleep"); Serial.flush();
 	delay(100);
 	LowPower.powerDown(SLEEP_2S, ADC_OFF, BOD_OFF); 
-	
 }
 
 void display(int m)
@@ -35,34 +40,7 @@ void clear_display() {
 	display(0);
 }
 
-int x=0;
-int l=1;
-int n=0;
-int m=0;
-int start_loop_time = 0;
-
 void loop() {
-	const int x=digitalRead(reed_pin);
-	const bool changed=(x!=l);
-
-	if (changed) {
-		n++;
-		last_time_change=millis();
-		m=n/2;
-		display(m);
-	}
-	
-	int time_since_last_change = millis() - last_time_change;
-	int measuring_time = millis() - start_loop_time;
-	
-	if (measuring_time > 1000 && time_since_last_change > 4000) {
-		display(m);
-		delay(250);
-		clear_display();
-		sleepNow();		
-		start_loop_time = millis();
-	}
-	else
-		delay(50);
-	l=x;
+	display(n);
+	delay(250);
 }
