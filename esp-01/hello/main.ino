7e6875a4b057606fac917b4715f434e7bb947eba
Julien5 <julien_bourgeois@yahoo.fr>
2018-10-28 12:31:18 +0100

    - function sendCommand which wait for "ERROR" (untested) or "OK"

--- esp-01/hello/main.ino
+++ esp-01/hello/main.ino
@@ -2,18 +2,41 @@
 
 AltSoftSerial Altser;
 
+boolean sendCommand(String command)
+{
+  Serial.println(("send:")+command);
+  const String s = command+"\r\n"; 
+  Altser.write(s.c_str());
+  long int now = millis();
+  long int deadline = now + 1000;
+  String response;
+  bool received = false;
+  while(millis()<deadline && !received) {
+    while(Altser.available()) {
+      char r=Altser.read();
+      response+=r;
+      if (r=='\n') {
+	if ((response == "OK\r\n") || (response == "ERROR\r\n"))
+	  received=true;
+	else {
+	  response="";
+	}
+	break;
+      }
+    }
+  }
+  Serial.print("received:");
+  Serial.println(response);
+  return received;  
+}
+
 void setup()
 {
   Serial.begin(9600);
   while(!Serial);
   Altser.begin(9600);
+  sendCommand("AT");
 }
 
-int x=0;
-
 void loop() {
-  while(Altser.available())
-    Serial.write(Altser.read());
-  delay(250);
-  Altser.write("AT\r\n");
 }
