30479184357ae76ec79d2bd065411257d61ec1d5
Julien5 <julien_bourgeois@yahoo.fr>
2018-11-11 19:32:15 +0100

    * work on parse of wifi string

--- esp-01/hello/tests/test.cpp
+++ esp-01/hello/tests/test.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 #include "parse.h"
+#include "wifi.h"
 
-int main(int argc, char ** argv) {
-  std::cout << "test\n";
-  int exitcode=parse::test();
-  std::cout << "exit code:" << exitcode << std::endl;
+int display(int exitcode) {
   if (exitcode!=0)
     std::cout << "bad.\n";
   else
     std::cout << "good.\n";
+  if (exitcode!=0)
+    exit(exitcode);
   return exitcode;
+}
+
+int main(int argc, char ** argv) {
+  std::cout << "test\n";
+  display(parse::test());
+  display(wifi::test());
+  return 0;
 }
