307af4511450c93b40bbfa027fd839ad8c68b97c
Julien5 <julien_bourgeois@yahoo.fr>
2019-01-04 14:53:24 +0100

    * use portable types (uint8_t, etc.)

--- esp-01/hello/statistics.h
+++ esp-01/hello/statistics.h
@@ -1,30 +1,42 @@
 #pragma once
 
+#include <cstdint>
+
 namespace types
 {
-  using minute = unsigned int;
-  using count = unsigned char;
-  using milli = unsigned int;
+  using minute = uint16_t;
+  using count = uint8_t;
+  using milli = uint16_t;
 }
+
 constexpr int NMILLIS=2;
 constexpr int NMINUTES=3;
 constexpr int NDATA=(sizeof(types::minute)+sizeof(types::count))*NMINUTES
   + sizeof(types::milli)*NMILLIS + 1;
 
+/* 
+   as i understand now, packing the data in a char* was not really needed.
+   struct alignement on 8-bit arduino yields the same memory layout 
+   struct alignement on 32-bit pc yields more memory usage.
+
+   but packing in char is still useful for deserilization on rpi.
+*/
+
 class statistics {
 private:
-  char data[NDATA];
+  uint8_t data[NDATA];
   void clear();
 
 public:
   statistics();
+  statistics(uint8_t * data); // of length NDATA
   void tick();
-  void save();
-  bool load();
+  void save_eeprom();
+  bool load_eeprom();
   int total();
   int minute_count();
   void get_minute(const int m, types::minute *, types::count *);
-  char *getdata(int * Lout);
+  uint8_t *getdata(int * Lout);
   bool operator==(const statistics& other);
   static int test();
 };
