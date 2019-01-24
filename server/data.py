7d72b4c9f1b5dbc9ba5f2e01a9d23fe4e564a1b8
Julien5 <julien_bourgeois@yahoo.fr>
2019-01-24 12:53:17 +0100

    * clean up data.py

--- server/data.py
+++ server/data.py
@@ -10,88 +10,79 @@ import statistics;
 import datetime;
 import json;
 
-conn=None;
-sqlite=None;
-
-csv=dict();
-millis=dict();
-def merge(t,j):
-    global csv;
-    date=t;
-    data=json.loads(j);
-    d="";
-    h="";
-    sent_time=t.hour*60+t.minute;
-    back_minute=-1;
-    for m in data["minutes"]:
-        # d += str(m["minute"])+","+str(m["count"])+"\n";
-        generated_time = m["minute"];
-        back_minute = generated_time;
-        generated_date = date;
-        if generated_time>sent_time:
-            generated_date = date.replace(day=date.date().day-1);
-        key=generated_date.strftime("%Y-%m-%d");
-        if not key in csv:
-            csv[key]=dict();
-        csv[key][m["minute"]]=m["count"];
+class Data:
+    def __init__(self):
+        self.csv=dict();
+        self.millis=dict();
         
-    h=int(back_minute/60);
-    m=int(back_minute-h*60);
-    key=date.strftime("%Y-%m-%d")+"-"+str(h)+":"+str(m)+"-"+str(len(millis));
-    millis[key]=list();
-    m0=None;
-    for m in data["millis"]:
-        if m0 and m:
-            millis[key].append(m0-m);
-        m0=m;
+    def merge(self,httpdate,jstring):
+        data=json.loads(jstring);
+        sent_time=httpdate.hour*60+httpdate.minute;
+        back_minute=-1;
+        for m in data["minutes"]:
+            generated_time = m["minute"];
+            back_minute = generated_time;
+            generated_date = httpdate;
+            if generated_time>sent_time:
+                generated_date = httpdate.replace(day=httpdate.date().day-1);
+            key=generated_date.strftime("%Y-%m-%d");
+            if not key in self.csv:
+                self.csv[key]=dict();
+            self.csv[key][m["minute"]]=m["count"];
+            
+        h=int(back_minute/60);
+        m=int(back_minute-h*60);
+        key=httpdate.strftime("%Y-%m-%d")+"-"+str(h)+":"+str(m)+"-"+str(len(self.millis));
+        self.millis[key]=list();
+        m0=None;
+        for m in data["millis"]:
+            if m0 and m:
+                self.millis[key].append(m0-m);
+            m0=m;
+            
+    def process(self,hex,t):
+        t1=datetime.datetime.strptime(t,"%Y-%m-%d %H:%M:%S.%f");
+        t0=datetime.datetime.strptime("2019-01-19 18:00","%Y-%m-%d %H:%M");
+        if not t1>t0:
+            return;
+        self.merge(t1,hamster.statistics.asJson(hex));
 
-def process(hex,t):
-    t1=datetime.datetime.strptime(t,"%Y-%m-%d %H:%M:%S.%f");
-    t0=datetime.datetime.strptime("2019-01-19 18:00","%Y-%m-%d %H:%M");
-    if not t1>t0:
-        return;
-    merge(t1,hamster.statistics.asJson(hex));
-
-def getdb(filename):
-    global sqlite;
-    global conn;
-    create=not os.path.exists(filename);
-    conn = sqlite3.connect(filename);
-    sqlite = conn.cursor();
+class Sql:
+    def __init__(self,filename):
+        self.conn = sqlite3.connect(filename);
+        self.sqlite = self.conn.cursor();
+    def select(self,statement):
+        self.sqlite.execute(statement)
+        return self.sqlite.fetchall();
     
 def main():
-    global sqlite;
-    global conn;
     global csv;
     
     filename="sqlite.db";
-    getdb(filename);
-    
-    cur = conn.cursor()
-    cur.execute("SELECT req,time FROM requests")
-    rows = cur.fetchall()
-    for row in rows:
+    sql=Sql(filename);
+    data=Data();
+    for row in sql.select("SELECT req,time FROM requests"):
         (req,t)=row;
         if isinstance(req,bytes):
-            process(req.hex(),t);
-    for date in csv:
+            data.process(req.hex(),t);
+
+    for date in data.csv:
         filename=date+".csv";
         f=open(filename,'w');
         d = "";
-        for m in sorted(csv[date]):
-            #print(date, m);
-            d += str(m)+","+str(csv[date][m])+"\n";
-        #print(d);
+        for m in sorted(data.csv[date]):
+            d += str(m)+","+str(data.csv[date][m])+"\n";
         f.write(d);
-    for k in millis:
+    for k in data.millis:
         filename="millis/"+str(k)+".csv";
         f=open(filename,'w');
         d = "";
         i=1;
-        for m in millis[k]:
+        for m in data.millis[k]:
             d += str(i)+","+str(m)+"\n";
             i = i+1;
         f.write(d);
+    
 
 if __name__ == "__main__":
     main();
