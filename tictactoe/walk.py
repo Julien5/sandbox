8312a9896396b55d807eed7e86393e6cf98a7035
Julien5 <julien_bourgeois@yahoo.fr>
2019-03-05 12:02:39 +0100

    detect winner and stop the search

--- tictactoe/walk.py
+++ tictactoe/walk.py
@@ -1,38 +1,68 @@
 #!/usr/bin/env python3
 
+N=9;
+
 class Board:
-    # 1,2,3
-    # 8,9,4
-    # 7,6,5
-    def __init__(self, X, O):
-        self.X = set(X);
-        self.O = set(O);
+    # 0,1,2
+    # 7,8,3
+    # 6,5,4
+    def __init__(self, B=' '*N):
+        self.board = B;
 
+    def _winner(self,I):
+        L=[self.board[i] for i in I];
+        l0 = L[0]
+        if l0 == ' ':
+            return None;
+        for l in L:
+            if l != l0:
+                return None;
+        return l0;
+
+    def winner(self):
+        b=self.board;
+        II=list();
+        II.append({0,8,4});
+        II.append({0,7,6});
+        II.append({1,8,5});
+        II.append({2,3,4});
+        II.append({0,1,2});
+        II.append({7,8,3});
+        II.append({6,5,4});
+        II.append({6,8,2});
+        for I in II:
+            w=self._winner(I);
+            if w:
+                return w;
+        return None;
+            
+    def stop(self):
+        w=self.winner();
+        return self.winner() != None;
+
+    def free(self):
+        if self.stop():
+            return [];
+        return [i for i in range(len(self.board)) if self.board[i]==' '];
+    
+    def child(self,position):
+        assert(position in self.free());
+        assert(self.board[position] == ' ');
+        xturn=self.board.count('x') == self.board.count('o');
+        sign='o';
+        if xturn:
+            sign='x';
+        L=list(self.board);
+        L[position]=sign;
+        b=''.join(L);
+        return Board(b);
+    
     def children(self):
-        N=3;#9;
-        if len(self.X) == len(self.O):
-            # X's turn.
-            for i in range(N):
-                p = i+1;
-                if p in self.X or p in self.O:
-                    continue;
-                else:
-                    X=set(self.X);
-                    X.add(p);
-                    yield Board(X,self.O);
-        else:
-            # O's turn
-            for i in range(N):
-                p = i+1;
-                if p in self.X or p in self.O:
-                    continue;
-                else:
-                    O=set(self.O);
-                    O.add(p);
-                    yield Board(self.X,O);
-
+        for p in self.free():
+            yield self.child(p);
+            
     def string(self):
-        return "X:"+str(self.X)+"O:"+str(self.O);
+        return self.board;
 
 
 def walk(b):
@@ -41,7 +71,7 @@ def walk(b):
         walk(c);
         
 def main():
-    b=Board(set(),set());
+    b=Board();
     walk(b);
 
 if __name__ == '__main__':
