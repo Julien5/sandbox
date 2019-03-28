27ea6e79eb27252e81fcfa2bdb6ce0909223cdb5
Julien5 <julien_bourgeois@yahoo.fr>
2019-03-28 22:34:36 +0100

    another gradient, for a single neuron

--- tictactoe/ann.py
+++ tictactoe/ann.py
@@ -3,32 +3,38 @@
 import math;
 
 def f(x):
-    return 0.5*(1+math.tanh(x));
+    c=1;
+    return 0.5*(1+math.tanh(c*x));
 
 def df(x):
-    return 0.5*(1-math.pow(math.tanh(x),2));
+    c=1;
+    return c*0.5*(1-math.pow(math.tanh(c*x),2));
 
 def dJ(X,T,A):
     a = A[0];
     b = A[1];
     da = sum([X[i]*df(a*X[i]+b)*(f(a*X[i]+b)-T[i]) for i in range(len(X))]);
     db = sum([     df(a*X[i]+b)*(f(a*X[i]+b)-T[i]) for i in range(len(X))]);
-    #print(da,db);
     return [da,db];
+
+def J(X,T,A):
+    a = A[0];
+    b = A[1];
+    return sum([math.pow((f(a*X[i]+b)-T[i]),2) for i in range(len(X))]);
 
 def distance(A,Aold):
     return sum([math.pow(A[i]-Aold[i],2) for i in range(len(A))]);
 
 def learn(X,T):
-    A=[1,1];
+    A=[-0.8,0];
     mu=0.5;
-    epsilon=mu/100;
+    epsilon=mu/10;
     Aold = None;
     while not Aold or distance(A,Aold)>epsilon:
         d = dJ(X,T,A);
         A = [A[i] - mu*d[i] for i in range(len(A))];
-        A[0] = A[0]/math.fabs(A[0]);
-        print(A);
+        #A[0] = A[0]/math.fabs(A[0]);
+        print(-A[1]/A[0],J(X,T,A),A);
     return A;
 
 def main():
