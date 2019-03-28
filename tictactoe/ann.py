7b57192b27e87442ce1576174c13eba0510003a0
Julien5 <julien_bourgeois@yahoo.fr>
2019-03-28 15:46:33 +0100

    gradient descent

--- tictactoe/ann.py
+++ tictactoe/ann.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python3
 
+import math;
+
 def f(x):
-    return x*x;
+    return math.cos(x*x);
+
+def df(x):
+    return -2*x*math.sin(x*x);
 
 def gradient(f):
-    return (0,f(0));
+    x = 0.1;
+    xold = None;
+    mu=0.10;
+    epsilon = mu/1000.0;
+    niter=0;
+    while (not xold) or math.fabs(x-xold)>epsilon:
+        xold = x;
+        x = x - mu*df(x);
+        niter = niter + 1;
+    print("niter:",niter);
+    return (x,f(x));
 
 def main():
     (x,m)=gradient(f);
