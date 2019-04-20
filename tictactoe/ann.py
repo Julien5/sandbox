d214372eaf609ea67801408132f68baa28eba31a
Julien5 <julien_bourgeois@yahoo.fr>
2019-04-20 14:45:08 +0200

    vector ann learning

--- tictactoe/ann.py
+++ tictactoe/ann.py
@@ -4,7 +4,7 @@ import math;
 import numpy as np;
 
 def _f(x):
-    c=1;
+    c=10;
     return 0.5*(1+math.tanh(c*x));
 
 npf = None;
@@ -15,7 +15,7 @@ def f(x):
     return npf(x);
 
 def _df(x):
-    c=1;
+    c=10;
     return c*0.5*(1-math.pow(math.tanh(c*x),2));
 
 npdf = None;
@@ -25,61 +25,62 @@ def df(x):
         npdf=np.vectorize(_f);
     return npdf(x);
 
+def mult(A,B):
+    return np.dot(A,B);
+
+def norm(A):
+    return np.linalg.norm(A,2);
+
 def d(x,t,A,b):
-    return f(A*x+b)-t;
+    return f(mult(A,x)+b)-t;
 
 def fp(x,t,A,b):
-    return df(A*x+b);
+    return df(mult(A,x)+b);
 
 def dp(x,t,A,b):
     d_ = d(x,t,A,b);
     fp_ = fp(x,t,A,b);
-    return np.diag(d_)*fp_;
+    return mult(np.diag(d_),fp_);
 
 def dJ(X,T,A,b):
     I = range(len(X));
-    dA = sum([X[i]*np.transpose(dp(X[i],T[i],A,b)) for i in I]);
-    i=0;
-    print(X[i].shape)
-    print(dp(X[i],T[i],A,b).shape);
-    print(dA.shape);
-    print("--");
+    dA = sum([mult(dp(X[i],T[i],A,b),np.transpose(X[i])) for i in I]);
     db = sum([dp(X[i],T[i],A,b) for i in I]);
     return [dA,db];
 
 def J(X,T,A,b):
     I = range(len(X));
-    return sum([np.linalg.norm(f(A*X[i]+b)-T[i],2) for i in I]);
+    return sum([np.linalg.norm(f(mult(A,X[i])+b)-T[i],2) for i in I]);
 
 def distance(A,Aold):
-    return np.linalg.norm(A-Aold,2);
-
+    return norm(A-Aold);
+    
 def learn(X,T):
-    A=np.zeros((T[0].shape[0],X[0].shape[0]));
+    N=T[0].shape[0];
+    M=X[0].shape[0];
+    A=np.zeros((N,M));
     A[0,0]=1;
-    b=np.zeros(X[0].shape[0]);
-    print(A.shape,b.shape);
-    mu=1;
-    epsilon=mu/10;
-    Aold = None;
-    while not Aold or distance(A,Aold)>epsilon:
+    A[0,1]=0;
+    b=np.zeros(N);
+    mu=0.5;
+    while J(X,T,A,b)>0: 
         d = dJ(X,T,A,b);
+        d = d/norm(d);
         A = A - mu*d[0];
         b = b - mu*d[1];
-        #A[0] = A[0]/math.fabs(A[0]);
-        print(A.shape,b.shape);
-        return;
-        #print(J(X,T,A,b));
+        print(J(X,T,A,b));
+    print("A:",A," b:",b);
     return A;
 
 def main():
     X=[];
     T=[]; 
     for i in range(4):
-        x1=int(i&2>0);
-        x2=int(i&1>0);
-        X.append(np.array([x1,x2]));
-        T.append(np.array([int(x1 or x2)]));
+        x = np.zeros((2,1));
+        x[0] = int(i&2>0);
+        x[1] = int(i&1>0);
+        X.append(x);
+        T.append(np.array([int(x[0] or x[1])]));
     learn(X,T);
 
 if __name__ == '__main__':
