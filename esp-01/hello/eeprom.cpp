5376bda5b4fc1f8d426f521e5c9703fde2843eca
Julien5 <julien_bourgeois@yahoo.fr>
2019-12-24 23:21:17 +0100

    compiles, not even partially tested, esp8266 not started

--- esp-01/hello/eeprom.cpp
+++ esp-01/hello/eeprom.cpp
@@ -1,28 +1,132 @@
 #include "eeprom.h"
 #include "debug.h"
+#include "platform.h"
+
+#if !defined(ARDUINO) && !defined(ESP8266)
+#define DEVHOST
+#endif
 
 #ifdef ARDUINO
 #include <EEPROM.h>
-#else
-static char mem[512];
+// [magic number, length16, data, checksum on data]
+
+namespace arduino {
+  char readbyte(int addr) {
+    return EEPROM.read(addr);
+  }
+  void writebyte(int addr, char d) {
+    EEPROM.write(addr,d);
+  }
+}
+using namespace arduino;
+eeprom::eeprom(){};
 #endif
 
-char eeprom::read(int addr) {
-#ifdef ARDUINO
-  return EEPROM.read(addr);
-#else
-  if (addr<10)
-    printf("read %d : %#02x\n",addr,mem[addr]);
-  return mem[addr];
+#ifdef DEVHOST
+
+namespace x86 {
+  static char mem[4096];
+  char readbyte(int addr) {
+    if (addr<10)
+      printf("read %d : %#02x\n",addr,mem[addr]);
+    return mem[addr];
+  }
+  void writebyte(int addr, char d) {
+    if (addr<10)
+      printf("write %d : %#02x\n",addr,d);
+    mem[addr]=d;
+  }
+}
+eeprom::eeprom(){
+  // eeprom mock is volatile !
+  for(int k=0; k<sizeof(x86::mem)/sizeof(char);++k)
+    x86::writebyte(0,0);
+}
+using namespace x86;
 #endif
+
+#if defined (ARDUINO) || defined (DEVHOST)
+#define BYTEWISE
+#define MAGIC 86
+namespace bytewise {
+  bool eeprom_check(uint16_t *length, int *indx) {
+    int index=0;
+    uint8_t should_be_magic=readbyte(index++);
+    if (should_be_magic!=MAGIC)
+      return false;
+    if (length)
+      *length=0;
+    uint16_t L=0;
+    {
+      char _L[2]={};
+      _L[0]=readbyte(index++);
+      _L[1]=readbyte(index++);
+      L=*(uint16_t*)(&_L);
+      if (indx)
+	*indx=index;
+    }
+    uint8_t checksum=0;
+    for(int k=0; k<L; ++k,++index) {
+      uint8_t d=readbyte(index);
+      checksum+=d;
+    }
+    uint8_t checksum_saved=readbyte(index++);
+    if (checksum != checksum_saved)
+      return false;
+    if (length)
+      *length=L;
+    return true;
+  }
+  eeprom::length read(const eeprom::ram_address &dst, const eeprom::length &L) {
+    const eeprom::eeprom_address src=0;
+    eeprom::length ret=0;
+    int index=0;
+    if (!eeprom_check(&ret,&index))
+      return 0;
+    eeprom::ram_address addr=dst;
+    for(int k=0; k<xMin(ret,L); ++k) {
+      *addr=readbyte(index++);
+      addr++;
+    }
+    return ret;
+  }
+  eeprom::length write(const eeprom::ram_address &src, const eeprom::length &L) {
+    const eeprom::eeprom_address dst=0;
+    int index=0;
+    writebyte(index++,MAGIC);
+    
+    char * _L=(char*)&L;
+    writebyte(index++,*(_L++));
+    writebyte(index++,*(_L++));
+    
+    for(int k=0; k<L; ++index,++k)
+      writebyte(index,src[k]);
+    //writebyte(index++,checksum(src,L));
+    return L;
+  }
+}
+using namespace bytewise;
+#endif
+
+#ifdef ESP8266
+// use directly: spiflash_write from spiflash.h
+#include <sysparam.h>
+using namespace esp8266;
+namespace esp8266 {
+  eeprom::eeprom() {
+  }
+  eeprom::length read(const eeprom::ram_address &dst, const eeprom::length &L) {
+    return 0;
+  }
+  eeprom::length write(const eeprom::ram_address &src, const eeprom::length &L) {
+  }
+}
+#endif
+
+eeprom::length eeprom::read(const eeprom_address &src, const eeprom::ram_address &dst, const eeprom::length &L) {
+  return bytewise::read(dst,L);
 }
 
-void eeprom::write(int addr, char d) {
-#ifdef ARDUINO
-  EEPROM.write(addr,d);
-#else
-  if (addr<10)
-    printf("write %d : %#02x\n",addr,d);
-  mem[addr]=d;
-#endif
-}   
+eeprom::length eeprom::write(const eeprom_address &dst, const eeprom::ram_address &src, const eeprom::length &L) {
+  return bytewise::write(src,L);
+}
--- esp-01/hello/eeprom.h
+++ esp-01/hello/eeprom.h
@@ -1,7 +1,15 @@
 #pragma once
-
+#include <stdint.h>
 class eeprom {
 public:
-  char read(int addr);
-  void write(int addr, char d);
+  typedef uint8_t* ram_address; 
+  typedef uint16_t eeprom_address;
+  typedef uint16_t length;
+  eeprom();
+  // L is the number of bytes that can be written in ram_address.
+  // Returns the number of bytes effectively read.
+  length read(const eeprom_address &src, const ram_address &dst, const length &L);
+  // L is the number of bytes that can be read starting at ram_address.
+  // Returns the number of bytes effectively written.
+  length write(const eeprom_address &dst, const ram_address &src, const length &L);
 };
