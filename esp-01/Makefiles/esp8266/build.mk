f1a65df5bf2e15770efe6adf5f56d4faa75e13c4
Julien5 <julien_bourgeois@yahoo.fr>
2019-12-28 13:04:00 +0100

    * (build) port to esp8266

---
+++ esp-01/Makefiles/esp8266/build.mk
@@ -0,0 +1,27 @@
+ifndef SRCSDIR
+$(error SRCSDIR not set)
+endif
+OBJSDIR := /tmp/build/esp8266/$(NAME)
+
+# todo: remove duplicates.
+INCLUDE += -I$(SRCSDIR) $(XINCLUDE)
+
+SRCS := $(wildcard $(SRCSDIR)/*.cpp)
+OBJS := $(patsubst $(SRCSDIR)/%.cpp,$(OBJSDIR)/%.o,$(SRCS))
+
+.PHONY: dir
+
+dir:
+	mkdir -p $(OBJSDIR) 
+
+lib: dir $(OBJS) 
+	$(AR) rcs $(OBJSDIR)/lib$(NAME).a $(OBJS)
+
+exe: dir $(OBJS)
+	$(CXX) $(LDFLAGS) $(OBJS) $(XLIBS) $(LIBS) -o $(NAME).esp8266
+
+$(OBJS): $(OBJSDIR)/%.o : $(SRCSDIR)/%.cpp
+	$(CXX) $(DEFINES) $(CXXFLAGS) $(XINCLUDE) $(INCLUDE) -c -o $@ $<
+
+clean: dir
+	find $(OBJSDIR) -type f -delete -print
--- esp8266/esp-open-rtos/temperature/http.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <stdint.h>
-namespace http {
-  typedef void (*get_callback)(uint8_t *data, const int16_t length);
-  void get(get_callback callback);
-  void post(uint8_t *data, const uint16_t length, get_callback callback);
-}
