2ba74ab2192f683e2aedcea8ad9dc3a1a94780de
Julien5 <julien_bourgeois@yahoo.fr>
2020-01-03 00:19:06 +0100

    * fix rtcmemory

--- embedded/compteur/application_esp8266.cpp
+++ embedded/compteur/application_esp8266.cpp
@@ -5,17 +5,44 @@
 #include "rtcmemory.h"
 #include "sleep.h"
 
+#define LEN (512-36)
+
+// dumps raw memory in hex byte and printable split format
+void dump(const unsigned char *data_buffer, const unsigned int length) {
+  unsigned char byte;
+  unsigned int i, j;
+  for(i=0; i < length; i++) {
+    byte = data_buffer[i];
+    printf("%02x ", data_buffer[i]);  // display byte in hex
+    if(((i%16)==15) || (i==length-1)) {
+      for(j=0; j < 15-(i%16); j++)
+	printf("   ");
+      printf("| ");
+      for(j=(i-(i%16)); j <= i; j++) {  // display printable bytes from line
+	byte = data_buffer[j];
+	if((byte > 31) && (byte < 127)) // outside printable char range
+	  printf("%c", byte);
+	else
+	  printf(".");
+      }
+      printf("\n"); // end of the dump line (each line 16 bytes)
+    } // end if
+  } // end for
+}
+
+
 void maintask(void *pvParameters)
 {
   printf("maintask\n");
   // printf("eeprom test: %d\n",eeprom::test());
   rtcmemory R;
-  char data0[12]={0};
+  unsigned char data0[LEN]={0};
   if (!R.read(data0,sizeof(data0)))
     printf("could not read\n");
-  printf("data0:%s\n",data0);
-  char data1[12]="bonjour";
-  printf("data1:%s\n",data1);
+  dump(data0,LEN);
+  char data1[LEN]={0};
+  for(unsigned k=0;k<LEN;++k)
+    data1[k]=0xFF;
   if (!R.write(data1,sizeof(data1)))
     printf("could not write\n");
   sleep S;
