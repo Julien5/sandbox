2d7e14d0d0532e54821a17e2115fe4a0e3ab8c43
Julien5 <julien_bourgeois@yahoo.fr>
2020-01-13 16:37:50 +0100

    * progress on Makefiles for arduino

--- embedded/Makefiles/arduino/common.mk
+++ embedded/Makefiles/arduino/common.mk
@@ -1,17 +1,16 @@
 include ../settings
-
 include settings
 include common
-include LowPower
-INCLUDE += -I/usr/share/arduino/libraries/EEPROM/
 
 NAME:=common
 
 all: deps lib
 
 deps:
-	make -C $(MAKEFILESDIR)/arduino -f LowPower lib
+	make -C $(MAKEFILESDIR)/arduino -f LowPower.mk lib
 
+INCLUDE += -I/usr/share/arduino/libraries/EEPROM/
 SRCSDIR := $(DIREMBEDDED)/$(NAME)
+
 include build.mk
 
