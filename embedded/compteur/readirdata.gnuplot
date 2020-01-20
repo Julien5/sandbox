6987c59a05568e01bfff65dbbb50756d07f8fe5e
Julien5 <julien_bourgeois@yahoo.fr>
2020-01-20 07:02:17 +0100

    * signal plotting python scripts

--- embedded/compteur/readirdata.gnuplot
+++ embedded/compteur/readirdata.gnuplot
@@ -6,4 +6,6 @@ set output 'ir.png'
 set style line 1 lc rgb '#0060ad' lt 1 lw 2 pt 7 pi -1 ps 1.5
 set pointintervalbox 3
 set grid;
+set yrange [50:80]
+set xrange [2040:2040.5]
 plot "data.csv" with lines ls 1 
--- embedded/compteur/readirdata.py
+++ embedded/compteur/readirdata.py
@@ -3,14 +3,45 @@
 def chunks(data,L):
     ret=list();
     for n in range(int(len(data)/L)):
-        ret.append(data[n*L:(n+1)*L-1]);
+        c = data[n*L:(n+1)*L];
+        ret.append(c);
     return ret;
 
-def read(filename):
-    D8=chunks(open(filename,'r').read(),8);
-    for d8 in D8:
-        c2 = chunks(d8,2);
-        number = int("".join(c2[::-1]), 16)
-        print(number);
+def stringify(A):
+    if type(A) is list:
+        return "\t".join([str(a) for a in A]);
+    return str(A);
 
-read("/tmp/irdata3");
+def write(A,filename):
+    f=open(filename,'w');
+    f.write("\n".join([stringify(a) for a in A]));
+
+xprev = None;
+yprev = None;
+def filter(x):
+    global xprev,yprev;
+    if not xprev:
+        xprev = x;
+        yprev = x;
+    if x > 75 or x < 60:
+        x=67;        
+    alpha=0.5;
+    y=alpha*yprev + (1-alpha)*x;
+    xprev=x;
+    yprev=y;
+    return y;
+
+def read_hex(filename):
+    ret=list();
+    t=0;
+    for hexline in open(filename,'r').readlines():
+        D8=chunks(hexline.strip(),8);
+        for d8 in D8:
+            assert(len(d8)==8);
+            c2 = chunks(d8,2);
+            x = int("".join(c2[::-1]), 16)
+            ret.append([t/1000,x,filter(x)]);
+            t+=8;
+    return ret;
+        
+write(read_hex("/tmp/irdata"),'data.csv');
