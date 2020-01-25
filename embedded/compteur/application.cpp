95d953872220b5922c45556fa3c240c8db38ac5c
Julien5 <julien_bourgeois@yahoo.fr>
2020-01-25 12:58:45 +0100

    * write IR data

--- embedded/compteur/application.cpp
+++ embedded/compteur/application.cpp
@@ -2,6 +2,16 @@
 #include "debug.h"
 #include "time.h"
 #include "sdcard.h"
+#include "stdint.h"
+
+#ifdef ARDUINO
+#include "Arduino.h"
+uint16_t analogRead() {
+  return analogRead(0);
+}
+#endif
+
+sdcard sd;
 
 void application::setup() {
 #ifdef ARDUINO
@@ -9,15 +19,31 @@ void application::setup() {
   while (!Serial) { }
   Serial.println("@START");
 #endif
-  sdcard sd;
   sd.init();
   sd.info();
   const char * d = "ffff.ggg";
   sd.write("foo.txt",(uint8_t*)d,strlen(d));
 }
 
+int data[256] = {0};
+int indx=0;
+
 void application::loop()
-{
-  DBG("mem:%d\r\n",debug::freeMemory());
-  time::delay(250);
+{ 
+  int a=analogRead();
+  data[indx++]=a;
+  DBG("data[%d]=%d\r\n",indx,a);
+
+  if (indx>=sizeof(data)/sizeof(int)) {
+    auto ms = time::since_reset();
+    int sec = ms/1000;
+    char filename[12];
+    sprintf(filename,"%d.bin",sec);
+    DBG("writing %s\r\n",filename);
+    sd.write(filename,(uint8_t*)data,sizeof(data));
+    indx=0;
+    DBG("mem:%d\r\n",debug::freeMemory()); 
+  }
+  
+  time::delay(150);
 }
