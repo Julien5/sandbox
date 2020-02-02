ce13b150f98d74f5c14db2e6ccdd189eaa9fb35d
Julien5 <julien_bourgeois@yahoo.fr>
2020-02-02 19:28:17 +0100

    * started work on histogram, for devhost

--- embedded/compteur/application.cpp
+++ embedded/compteur/application.cpp
@@ -24,8 +24,67 @@ uint16_t analogRead() {
 #endif
 
 #ifdef DEVHOST
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+namespace devhost {
+  std::string read_file(const std::string &fileName)
+  {
+    std::ifstream ifs(fileName.c_str(), std::ios::in | std::ios::ate);
+    std::ifstream::pos_type s = ifs.tellg();
+    ifs.seekg(0, std::ios::beg);
+    std::vector<char> bytes(s);
+    ifs.read(bytes.data(), s);
+    return std::string(bytes.data(), s);
+  }
+
+  std::vector<std::string> split(const std::string &s, const std::string &sep) {
+    size_t beg=0;
+    size_t end=0;
+    std::vector<std::string> ret;
+    while(true) {
+      end=s.find(sep,beg);
+      if (end==std::string::npos)
+	break;
+      auto part=s.substr(beg,end-beg);
+      ret.push_back(part);
+      beg=end+1;
+      end=0;    
+    } 
+    return ret;
+  }
+
+  std::vector<std::string> lines(const std::string &s) {
+    auto ret=split(s,"\n");
+    return ret;
+  }
+
+  std::vector<uint16_t> numbers(const std::vector<std::string> &lines) {
+    std::vector<uint16_t> ret;
+    for(auto & line : lines) {
+      const auto P=split(line,"\t");
+      ret.push_back(std::stoi(P[1]));
+    }
+    return ret;
+  }
+
+  std::vector<uint16_t> s_numbers;
+  size_t counter=0;
+  void get_data() {
+    std::string s=read_file("sd-data/23/hex/data.csv");
+    s_numbers=numbers(lines(s));
+  }
+}
+
 uint16_t analogRead() {
-  return 1;
+  using namespace devhost;
+  if (s_numbers.empty()) {
+    get_data();
+  }
+  assert(!s_numbers.empty());
+  return s_numbers[(counter++) % s_numbers.size()];
+  
 }
 #endif
 
@@ -47,6 +106,7 @@ void application::setup() {
   auto err=adc_init(&config);
   DBG("err=%d\r\n",err);
 #endif
+  
   sd.init();
   sd.info();
   const char * d = "ffff.ggg";
@@ -57,19 +117,55 @@ uint16_t data[256] = {0};
 uint16_t indx=0;
 uint16_t counter=0;
 
+
+class histogram {
+  static constexpr size_t N=16;
+  uint16_t occurences[N];
+  uint8_t values[N];
+public:
+  int8_t index(const uint8_t &v) {
+    for(int k=0;k<N;++k) 
+      if (values[k]==v)
+	return k;
+    return -1;
+  }
+  void reg(const uint8_t &v) {
+    auto i=index(v);
+    if (i<0) {
+      for(int k=0;k<N;++k) 
+	if (occurences[k]==0) {
+	  i=k;
+	  break;
+	}
+    }
+    values[i]=v;
+    occurences[i]++;
+  }
+  void print() {
+    for(int k=0;k<N;++k) {
+      DBG("value: %d #=%d\r\n",values[k],occurences[k]);
+    }
+  }
+  
+};
+
+histogram h;
+
 void application::loop()
 { 
   int a=analogRead();
+  h.reg(a);
   data[indx++]=a;
   DBG("data[%d]=%d\r\n",indx,a);
-
-  if (indx>=sizeof(data)/sizeof(int)) {
+  if (indx>=sizeof(data)/sizeof(uint16_t)) {
     char filename[13]; // 8.3 => 8+1+3+1 (zero termination) => 13 bytes.
     sprintf(filename,"%08u.BIN",counter++);
     DBG("writing %s\r\n",filename);
     sd.write(filename,(uint8_t*)data,sizeof(data));
     indx=0;
-    DBG("mem:%d\r\n",debug::freeMemory()); 
+    DBG("mem:%d\r\n",debug::freeMemory());
+    h.print();
+    // exit(0);
   }
   time::delay(10);
 }
--- embedded/compteur/readirdata.gnuplot
+++ embedded/compteur/readirdata.gnuplot
@@ -8,11 +8,11 @@ set style line 2 lc rgb 'red' pt 7 ps 0.2 lt 1 lw 2
 set pointintervalbox 3
 set grid;
 set autoscale fix;
-set xtics 50
+set xtics 500
 
 #set yrange [30:170]
 # set xrange [1900:2400]
 # set xrange [14000:14320]
-# set xrange [1090:1110]
+set xrange [1100:1700]
 plot "data.csv" using 1:2 with points ls 2, \
      "data.csv" using 1:3 with lines ls 1
