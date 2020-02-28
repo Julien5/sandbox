7f78fc1f94599cb012fc151631af0da38a67a5c3
Julien5 <julien_bourgeois@yahoo.fr>
2020-02-28 14:52:36 +0100

    * test is a blink app, just to test build chain for esp-01

--- embedded/test/application.cpp
+++ embedded/test/application.cpp
@@ -1,61 +1,14 @@
 #include "application.h"
-
-#include <SPI.h>
-#include <SD.h>
-
-File myFile;
-
+#include "debug.h"
+#include "time.h"
 void application::setup() {
-  // Open serial communications and wait for port to open:
-  Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-
-
-  Serial.print("Initializing SD card...");
-
-  if (!SD.begin(10)) {
-    Serial.println("initialization failed!");
-    while (1);
-  }
-  Serial.println("initialization done.");
-
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
-  myFile = SD.open("test.txt", FILE_WRITE);
-
-  // if the file opened okay, write to it:
-  if (myFile) {
-    Serial.print("Writing to test.txt...");
-    myFile.println("aaa.bbb");
-    // close the file:
-    myFile.close();
-    Serial.println("done.");
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
-  }
-
-  // re-open the file for reading:
-  myFile = SD.open("test.txt");
-  if (myFile) {
-    Serial.println("test.txt:");
-
-    // read from the file until there's nothing else in it:
-    while (myFile.available()) {
-      Serial.write(myFile.read());
-    }
-    // close the file:
-    myFile.close();
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
-  }
 }
 
 void application::loop() {
-  // nothing happens after setup
+  debug::turnBuildinLED(true);
+  time::delay(1000);
+  debug::turnBuildinLED(false);
+  time::delay(1000);
 }
 
 
---
+++ embedded/test/main_esp8266.cpp
@@ -0,0 +1,17 @@
+#include "application.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void maintask_esp8266(void *pvParameters) {
+  while (1) {
+    application::loop();
+  }
+}
+
+extern "C" {
+  void app_main()
+  {
+    application::setup();
+    xTaskCreate(&maintask_esp8266, "maintask_esp8266", 1024*16, nullptr, 2, nullptr);
+  }
+}
