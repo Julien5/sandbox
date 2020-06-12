598acb9331fe3a491837b07ed8ee0cf1ded89b85
Julien5 <julien_bourgeois@yahoo.fr>
2020-06-12 12:00:04 +0200

    - debug::freeMemory from: https://playground.arduino.cc/Code/AvailableMemory/

--- embedded/common/src/debug.cpp
+++ embedded/common/src/debug.cpp
@@ -60,11 +60,37 @@ void debug::turnBuildinLED(bool on) {
     digitalWrite(LED_BUILTIN,LOW);
 }
 
-extern char *__brkval;
- 
+/* https://playground.arduino.cc/Code/AvailableMemory/ */
+#include <Arduino.h>
+extern unsigned int __heap_start;
+extern void *__brkval;
+struct __freelist {
+  size_t sz;
+  struct __freelist *nx;
+};
+extern struct __freelist *__flp;
+int freeListSize() {
+  struct __freelist* current;
+  int total = 0;
+  for (current = __flp; current; current = current->nx) {
+    total += 2; /* Add two bytes for the memory block's header  */
+    total += (int) current->sz;
+  }
+  return total;
+}
+int arduinoFreeMemory() {
+  int free_memory;
+  if ((int)__brkval == 0) {
+    free_memory = ((int)&free_memory) - ((int)&__heap_start);
+  } else {
+    free_memory = ((int)&free_memory) - ((int)__brkval);
+    free_memory += freeListSize();
+  }
+  return free_memory;
+}
+/* */
 int debug::freeMemory() {
-  char top;
-  return &top - __brkval;
+  return arduinoFreeMemory();
 }
 
 #endif
