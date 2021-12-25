7d120f33f1bce67c50aa0317a3edb69defdec62a
Julien5 <julien_bourgeois@yahoo.fr>
2021-12-25 22:21:01 +0100

    it works

--- test/cmake/app/src/app.cpp
+++ test/cmake/app/src/app.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include "core/core.h"
+#include "lib/lib.h"
 
 int main(int argc, char **argv) {
-    std::cout << "hello world, core:" << core::foo() << std::endl;
+    std::cout << "hello world, core:" << core::foo() << " bar:" << lib::bar() << std::endl;
     return 0;
 }
---
+++ test/cmake/lib/src/lib.cpp
@@ -0,0 +1,6 @@
+#include "lib/lib.h"
+#include "core/core.h"
+
+int lib::bar() {
+    return 2;
+}
