881dbbe3e0202c5b4d6a9420a741d22ea05c8847
Julien5 <julien_bourgeois@yahoo.fr>
2021-12-31 20:15:28 +0100

    fix cmake files for esp8266

--- embedded/cmake/esp8266/core/build.sh
+++ embedded/cmake/esp8266/core/build.sh
@@ -23,17 +23,9 @@ cat $FILENAME | grep hello_world_main.c | grep "\-o" | tr " " "\n" | grep "^\-" 
 
 echo grabing LFLAGS
 qopened=false
-cat $FILENAME | grep hello-world.elf | grep g++ | grep "\-o" | tr " " "\n"  | grep -v "^$" | egrep -v "(-o|hello)" | grep -v "xxxlibmain.a" | while read a; do
+cat $FILENAME | grep hello-world.elf | grep g++ | grep "\-o" | tr " " "\n"  | grep -v "^$" | egrep -v "(-o|hello)" | grep -v "libmain.a" | while read a; do
 	if [[ -f $BUILDDIR/$a ]]; then
 		echo $(realpath $BUILDDIR/$a)
-	elif [[ -f $BUILDDIR/$a ]]; then
-		echo $(realpath $BUILDDIR/$a)
-	elif [[ "$a" = \-u ]]; then
-		echo \"SHELL:$a
-		qopened=true
-	elif [[ "$a" = \-L ]]; then
-		echo \"SHELL:$a
-		qopened=true
 	elif [[ "$a" = "esp8266"*".ld" ]]; then
 		find $IDF_PATH $BUILDDIR -name "$a" | head -1
 		echo find $IDF_PATH $BUILDDIR -name "$a" 1>&2
@@ -41,15 +33,9 @@ cat $FILENAME | grep hello-world.elf | grep g++ | grep "\-o" | tr " " "\n"  | gr
 	elif [[ "$a" = *"xtensa-lx106-elf-g++"* ]]; then
 		echo -n
 	else
-		if $($qopened); then
-			# close
-			echo $a"\" "
-			qopened=false
-		else
-			echo $a
-		fi
+		echo $a
 	fi
 done | tr "\n" " " > $BUILDDIR/LFLAGS.txt
 
 echo set\(CORE_CFLAGS $(cat /tmp/esp82266/core/CFLAGS.txt)\) >  /tmp/esp82266/core/core.cmake
-echo set\(CORE_LFLAGS $(cat /tmp/esp82266/core/LFLAGS.txt)\) >> /tmp/esp82266/core/core.cmake
+echo set\(CORE_LFLAGS \"SHELL:$(cat /tmp/esp82266/core/LFLAGS.txt)\"\) >> /tmp/esp82266/core/core.cmake
--- embedded/cmake/esp8266/toolchain.cmake
+++ embedded/cmake/esp8266/toolchain.cmake
@@ -1,11 +1,2 @@
 set(ESP8266 1)
-add_definitions(-DESP8266=1)
-string(CONCAT cmd ${CMAKE_CURRENT_LIST_DIR} "/core/build.sh")
-execute_process(COMMAND ${cmd} WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
-file(STRINGS /tmp/esp82266/core/CFLAGS.txt FLAGS)
-set(CMAKE_C_FLAGS   "${FLAGS} -std=gnu99")
-set(CMAKE_CXX_FLAGS "${FLAGS} -std=c++11")
-file(STRINGS /tmp/esp82266/core/LFLAGS.txt CMAKE_EXE_LINKER_FLAGS)
 include(/opt/esp8266/esp8266-toolchain-espressif/ESP8266_RTOS_SDK/tools/cmake/toolchain-esp8266.cmake)
-
-
