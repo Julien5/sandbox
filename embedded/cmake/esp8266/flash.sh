701d04d4baa606e7fc4d725a028d2b2638c485f5
Julien5 <julien_bourgeois@yahoo.fr>
2022-01-01 10:50:52 +0100

    add commit

--- embedded/cmake/esp8266/flash.sh
+++ embedded/cmake/esp8266/flash.sh
@@ -17,8 +17,12 @@ fi
 ELFFILE=$1
 BINFILE=$ELFFILE.bin
 
+# needed ?
 export ESPPORT=/dev/ttyUSB0
 export ESPBAUD=460800
+
+# commands found building:
+# pushd /opt/esp8266/esp8266-toolchain-espressif/ESP8266_RTOS_SDK/ && git clean -dfx && popd && rm -Rf /tmp/build && mkdir -p /tmp/build && cmake -S . -B /tmp/build && make -C /tmp/build flash VERBOSE=1
 
 python $IDF_PATH/components/esptool_py/esptool/esptool.py --chip esp8266 elf2image --flash_mode dio --flash_freq 40m --flash_size 2MB --version=3 -o "$BINFILE" "$ELFFILE"
 
