0bf1fdbb52bb451d4ee6fbe4e76d01ae0e129fda
Julien5 <julien_bourgeois@yahoo.fr>
2022-01-02 14:08:34 +0100

    record test-03

--- embedded/compteur/simulation/tools/take-record.sh
+++ embedded/compteur/simulation/tools/take-record.sh
@@ -7,3 +7,4 @@ m=$((n+1))
 DIR=simulation/data/test-$(printf "%02d" $m)
 mkdir -p $DIR
 egrep "(common::analog::read)" /tmp/build_arduino/cmake/screenlog.0 | cut -f6 -d: | tr -d "\r" > $DIR/output.adc
+cp /tmp/build_arduino/cmake/screenlog.0 $DIR/
