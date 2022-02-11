2696bd56de0bf38a5f03e108f98028c34549c953
Julien5 <julien_bourgeois@yahoo.fr>
2022-02-11 21:03:19 +0100

    got it to work at 4 MHz at 38400 baud (failed at 57600)

--- embedded/cmake/arduino/knife.sh
+++ embedded/cmake/arduino/knife.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
-set -x
+#set -x
 
 SCRIPTDIR=$(realpath $(dirname $0))
 . $SCRIPTDIR/../catusb.sh
@@ -15,16 +15,16 @@ function ftdiport() {
 }
 
 function dude() {
-	avrdude -v -p atmega328p -c arduino -P/dev/$(ftdiport) -b115200 $@
+	avrdude -p atmega328p -c arduino -P/dev/$(ftdiport) -b38400 $@
 }
 
 function dude.isp() {
-	avrdude -v -p atmega328p -c stk500 -P/dev/$(ispport) -b57600 $@
+	avrdude -p atmega328p -c stk500 -P/dev/$(ispport) -b57600 $@
 }
 
 function burn.bootloader() {
 	# erase, unlock (3F == FF for the lock bytes), set fuse 
-	dude.isp -e -Ulock:w:0xFF:m -Ulfuse:w:0xFF:m -Uhfuse:w:0xDE:m -Uefuse:w:0xFD:m 
+	dude.isp -e -Ulock:w:0xFF:m -Ulfuse:w:0xDD:m -Uhfuse:w:0xDE:m -Uefuse:w:0xFD:m 
 
 	OPTIBOOTHEX=/tmp/optiboot/optiboot/bootloaders/optiboot/optiboot_atmega328.hex
 	# OPTIBOOTHEX=/tmp/arduino-1.8.19/hardware/arduino/avr/bootloaders/optiboot/optiboot_atmega328.hex
@@ -36,7 +36,8 @@ function burn.bootloader() {
 		fi
 		pushd optiboot/optiboot/bootloaders/optiboot
 		make clean
-		make AVR_FREQ=16000000L BAUD_RATE=115200 LED=B5 LED_START_FLASHES=5 LED_DATA_FLASH=1 atmega328
+		make AVR_FREQ=4000000L BAUD_RATE=38400 LED=B5 LED_START_FLASHES=5 LED_DATA_FLASH=1 atmega328
+		sleep 1
 		popd
 		popd
 	fi
