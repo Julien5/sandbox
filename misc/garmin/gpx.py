3f1308a5831a72dd93b2be3ee65dc8007b83d888
Julien5 <julien_bourgeois@yahoo.fr>
2022-08-07 23:18:05 +0200

    trying routing with 50-points routes

--- misc/garmin/gpx.py
+++ misc/garmin/gpx.py
@@ -26,6 +26,33 @@ def save_waypoints(W,filename):
 	f.write(gpx.to_xml());
 	f.close();
 
+def to_routepoint(w):
+	p=gpxpy.gpx.GPXRoutePoint();
+	p.latitude=w.latitude;
+	p.longitude=w.longitude;
+	p.name = "{:02d}".format(int(w.name));
+	p.symbol = w.symbol;
+	p.extensions=w.extensions;
+	# p = w;
+	return p;
+
+def waypoints_to_route(W):
+	gpx = gpxpy.gpx.GPX()
+	R = gpxpy.gpx.GPXRoute();
+	R.name="route";
+	for w in W:
+		R.points.append(to_routepoint(w))
+	gpx.routes.append(R);
+	return R;
+
+
+def save_route(R,filename):
+	gpx = gpxpy.gpx.GPX()
+	gpx.routes.append(R);
+	f=open(filename,'w');
+	f.write(gpx.to_xml());
+	f.close();	
+
 def restart(segment):
 	T = gpxpy.gpx.GPXTrackSegment()
 	start_index=None;
@@ -46,14 +73,15 @@ def restart(segment):
 		T.points.append(p);
 	return T;
 
-def simplify(seg):
+def simplify(seg,count=None):
 	infile="/tmp/simplify.input.gpx";
 	outfile="/tmp/simplify.output.gpx";
 	for f in [infile,outfile]:
 		if os.path.exists(infile):
 			os.remove(infile);
 	save_segment(seg,infile);
-	count=min(len(seg.points)/10,200);
+	if not count:
+		count=min(len(seg.points)/10,200);
 	cmd="gpsbabel -i gpx -f {} -x simplify,count={} -o gpx -F {}".format(infile,count,outfile);
 	print(cmd)
 	os.system(cmd);
@@ -65,21 +93,23 @@ def simplify(seg):
 	print("new length:",len(track.segments[0].points));
 	return track.segments[0];
 
+def add_alarm_extension(w,meters):
+	e = mod_etree.Element(f'wptx1:WaypointExtension');
+	proximity = mod_etree.SubElement(e,f'wptx1:Proximity');
+	proximity.text = str(float(meters))
+	w.extensions.append(e);
+
 def segment_to_waypoints(S,with_alarm=False):
 	W = [];
 	for i in range(len(S.points)):
 		s = S.points[i];
 		w = gpxpy.gpx.GPXWaypoint();
 		w.name = "{:03d}".format(i);
-		if with_alarm:
-			w.symbol = "City (Small)";
+		w.symbol = "City (Small)";
 		w.latitude=s.latitude;
 		w.longitude=s.longitude;
 		if with_alarm:
-			e = mod_etree.Element(f'wptx1:WaypointExtension');
-			proximity = mod_etree.SubElement(e,f'wptx1:Proximity');
-			proximity.text = "200.0";
-			w.extensions.append(e);
+			add_alarm_extension(w,200);	
 		W.append(w);
 	return W;
 
@@ -91,13 +121,15 @@ def main(filename):
 	assert(len(track.segments)==1);
 	segment=track.segments[0];
 	T = restart(segment);
-	save_segment(T,"restarted.gpx");
+	save_segment(T,"o-restarted.gpx");
 
 	S = simplify(T);
+	S50 = simplify(T,50);
+	R = waypoints_to_route(segment_to_waypoints(S50));
 	W = segment_to_waypoints(S);
 	Wa = segment_to_waypoints(S,True);
-	save_waypoints(W,"waypoints.gpx");
-	save_waypoints(Wa,"waypoints-alarm.gpx");	
+	save_waypoints(Wa,"o-waypoints-alarm.gpx");
+	save_route(R,"o-route.gpx");
 	
 if __name__ == '__main__':
 	print(sys.argv);
