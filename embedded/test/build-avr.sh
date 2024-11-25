4db53af2d8db19c028439cb3fcb4022ac16a8949
Julien5 <julien_bourgeois@yahoo.fr>
2024-11-25 06:12:16 +0100

    add compilation database

---
+++ embedded/test/build-avr.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+. $HOME/.bashrc 
+
+set -e
+set -x
+
+SCRIPTDIR=$(dirname $(realpath $0))
+cd ${SCRIPTDIR}
+
+dev.avr
+
+BUILDDIR=/tmp/builds/avr
+SOURCEDIR=$(realpath ..)
+TOOLCHAIN=${SOURCEDIR}/cmake/arduino/toolchain.cmake
+
+rm -Rf /tmp/builds
+cmake -B ${BUILDDIR} -S ${SOURCEDIR} --toolchain ${TOOLCHAIN}
+VERBOSE=1 make -C ${BUILDDIR}
+
+avrdude -p atmega328p -P /dev/ttyUSB0 -c arduino -b 57600 -D -U flash:w:/tmp/builds/avr/test/test.hex:i
