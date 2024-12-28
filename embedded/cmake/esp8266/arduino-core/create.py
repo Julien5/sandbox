fe04c1f277389f33d5b3add209e4f3036e8cf702
Julien Bourgeois <julien_bourgeois@yahoo.fr>
2024-12-28 12:04:01 +0100

    pass board and key from the command line

--- embedded/cmake/esp8266/arduino-core/create.py
+++ embedded/cmake/esp8266/arduino-core/create.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import re;
+import sys;
 
 def read(filename):
 	f=open(filename,'r');
@@ -31,23 +32,40 @@ def resolve(P,s):
 	for key in R:
 		assert(key in P)
 		ret=ret.replace("{"+key+"}",P[key]);
-		print(key,"->",P[key]);
+		## print(key,"->",P[key]);
 	return resolve(P,ret);
+
+def subkeys(P,m,direct=True):
+	ret=list();
+	for k in P:
+		if not k.startswith(m):
+			continue;
+		if not direct:
+			ret.append(k);
+			continue;
+		if not "." in k[len(m)+1:]:
+			ret.append(k);
+	return ret;
 	
 def main():
-	K=re.findall(r'{\S+}', "aa.ss={foo.bar}d/ {ff.dd}/bar= dd");
-	print(K);
-	#return
-	#K=re.findall(r'=\S+', "aa.ss={foo.bar}/bar = d");
-	#print(K);
-	#return;
+	#coredir="/home/julien/delme/esp8266";
 	coredir="/opt/esp8266-toolchain/Arduino-3.1.2";
+	board="nodemcu";
+	key="recipe.cpp.o.pattern"
+	if len(sys.argv) > 1:
+		coredir=sys.argv[1];
+		board=sys.argv[2];
+		key=sys.argv[3];
 	platform=f"{coredir:s}/platform.txt"
 	boards=f"{coredir:s}/boards.txt"
 	P=read(platform);
 	B=read(boards);
+	direct=True;
+	menus=dict();
 	for b in B:
-		if b.startswith("nodemcu."):
+		if b.startswith("menu."):
+			menus[b]=B[b];
+		if b.startswith(board+"."):
 			unb=".".join(b.split(".")[1:])
 			if unb in P:
 				if unb == "name":
@@ -57,22 +75,19 @@ def main():
 				print("B:",B[b]);
 				assert(False)
 			P[unb]=B[b];
-			#print("from B:",unb,B[b]);
-	# override
 	P["compiler.path"]="";
 	P["build.f_cpu"]="80000000L"
 	P["runtime.ide.version"]="10612"
 	P["build.arch"]="ESP8266"
-	P["build.path"]="/opt/esp8266-toolchain/Arduino-3.1.2/cores/esp8266"
-	P["runtime.platform.path"]="/opt/esp8266-toolchain/Arduino-3.1.2";
+	P["build.path"]=coredir+"/cores/esp8266"
+	P["runtime.platform.path"]=coredir;
 	P["_id"]="generic";
 	# remove build.opt
 	P["build.opt.flags"]="";
-	for k in ["recipe.cpp.o.pattern"]:
-		v=resolve(P,P[k]);
-		print(f"{k:s}=={v:s}");
-		print("unresolved:",re.findall(r'{\S+}',v));
-	#print(P["compiler.cpp.cmd"]);
-
+	
+	v=resolve(P,P[key]);
+	print(f"{v:s}");
+	print("unresolved:",re.findall(r'{\S+}',v));
+	
 if __name__ == "__main__":
 	main();
