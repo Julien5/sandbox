0ad4b47a4f04e9b157de028cb4a20e556f1ad334
Julien5 <julien_bourgeois@yahoo.fr>
2025-02-13 04:49:12 +0100

    add script that builds the arduino core and sdfat with cmake

---
+++ embedded/cmake/esp8266/arduino-core/make-core.sh
@@ -0,0 +1,108 @@
+#!/usr/bin/env bash
+
+set -e
+# set -x
+
+function init() {
+	SCRIPTDIR=$(realpath $(dirname $0))
+	. $HOME/.profile
+}
+
+function fixup() {
+	R1=https://raw.githubusercontent.com/esp8266/Arduino/refs/heads/master
+	R2=https://raw.githubusercontent.com/alexCajas/esp8266RTOSArduCore/refs/heads/main
+
+	for a in pgmspace.h Print.h Print.cpp; do 
+		wget "${R1}/cores/esp8266/${a}" -O ${COREDIR}/cores/esp8266/${a} &> /dev/null
+	done 
+
+	rm -Rf /tmp/patch
+	mkdir -p /tmp/patch/include/sys/
+	cp $SCRIPTDIR/pgmspace.h /tmp/patch/include/sys/
+	touch /tmp/patch/include/sys/_tz_structs.h
+
+	#wget "${R2}/cores/esp8266/pgmspace.h" -O /tmp/patch/include/sys/pgmspace.h &> /dev/null
+	#wget "${R2}/cores/esp8266/Print.h" -O /tmp/patch/include/Print.h &> /dev/null
+}
+
+function test-esp8266() {
+	#rm -Rf /tmp/builds/esp8266/
+	dev.esp8266
+	#dev.esp8266.compiler 10.3.0-arduino
+	cd ~/delme
+	EXE=$HOME/projects/sandbox/embedded/cmake/esp8266/arduino-core/cmakify/src/main.py
+	${EXE} --board nodemcu --key recipe.cpp.o.pattern ${COREDIR}
+	echo
+	${EXE} --board nodemcu --key recipe.c.o.pattern ${COREDIR}
+	for k in FLAGS DEFINES INCLUDES; do
+		echo "* C ${k}"
+		${EXE} --board nodemcu --key recipe.c.o.pattern --filter ${k} ${COREDIR}
+		echo "* CPP ${k}"
+		${EXE} --board nodemcu --key recipe.cpp.o.pattern --filter ${k} ${COREDIR}
+		echo
+	done
+
+	${EXE} --board nodemcu --key compiler.c.cmd ${COREDIR}
+
+	${EXE} --board nodemcu --key compiler.cpp.cmd ${COREDIR}
+	echo "***"
+	${EXE} --board nodemcu --key recipe.c.combine.pattern ${COREDIR}
+	echo "***"
+	${EXE} --board nodemcu --toolchain ${COREDIR}
+
+	echo "***"
+	${EXE} --board nodemcu --lib ${COREDIR}/libraries/SPI ${COREDIR}
+	${EXE} --board nodemcu --lib ~/delme/SdFat ${COREDIR}
+
+	rm -Rf /tmp/cmake-test
+	mkdir -p /tmp/cmake-test
+	cd /tmp/cmake-test
+	mkdir -p {toolchain,core,SPI,sdfat}
+	${EXE} --board nodemcu --toolchain ${COREDIR} > toolchain/toochain.cmake
+	cp ${SCRIPTDIR}/root.cmake CMakeLists.txt
+	for d in core SPI sdfat; do
+		printf "add_subdirectory(%s)\n" "${d}" >> CMakeLists.txt
+	done
+	fixup
+	COREDEFINES=$(${EXE} --board nodemcu --key recipe.cpp.o.pattern ${COREDIR} --filter DEFINES | cut -b3- | xargs)
+	COREDEFINES=$COREDEFINES" ""$(${EXE} --board nodemcu --key menu.ip.lm2f.build.lwip_flags ${COREDIR} --filter DEFINES | cut -b3- | xargs)"
+	COREDEFINES="$COREDEFINES ""$(${EXE} --board nodemcu --key menu.mmu.3232.build.mmuflags ${COREDIR} --filter DEFINES | cut -b3- | xargs)"
+	COREINCLUDES="$(${EXE} --board nodemcu --key recipe.cpp.o.pattern --filter INCLUDES ${COREDIR} | cut -b3- | xargs)"
+	COREINCLUDES="$COREINCLUDES ${COREDIR}/cores/esp8266  ${COREDIR}/libraries/SPI   ${COREDIR}/libraries/Wire"
+
+	LWIPINCLUDES=${COREDIR}/tools/sdk/"$(${EXE} --board nodemcu --key menu.ip.lm2f.build.lwip_include ${COREDIR})"
+	#  /opt/esp8266-toolchain/ESP8266_RTOS_SDK/components/lwip/port/esp8266/include 
+	${EXE} --board nodemcu --lib ${COREDIR}/cores/esp8266 ${COREDIR} \
+		   --add-include-directories \
+		   /tmp/patch/include \
+		   $COREINCLUDES \
+		   ${COREDIR}/variants/nodemcu \
+		   $LWIPINCLUDES \
+		   --add-defines ${COREDEFINES} \
+		   > core/CMakeLists.txt
+	${EXE} --board nodemcu --lib ${COREDIR}/libraries/SPI ${COREDIR} > SPI/CMakeLists.txt
+	${EXE} --board nodemcu --lib ~/delme/SdFat ${COREDIR} \
+		   --add-include-directories \
+		   /tmp/patch/include \
+		   $COREINCLUDES \
+		   ${COREDIR}/variants/nodemcu \
+		   --add-link-libraries esp8266 \
+		   > sdfat/CMakeLists.txt
+	mkdir -p build
+	#rm -Rf build/*
+	dev.esp8266
+	cmake -S . -B build --toolchain toolchain/toochain.cmake
+	cd build
+	2>&1 make -j8 SdFat # VERBOSE=1
+}
+
+function main() {
+	COREDIR=$HOME/delme/Arduino-master
+	test-esp8266
+	#download-esp8266-arduino-core
+	#compare
+}
+
+init
+main "$@"
+echo all good
