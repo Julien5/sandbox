9361c4728071734c9e5241d2cfeec0ae642bc732
Julien5 <julien_bourgeois@yahoo.fr>
2025-03-03 11:35:51 +0100

    shuffle a minesweeper grid

--- test/rust/minesweeper/src/main.rs
+++ test/rust/minesweeper/src/main.rs
@@ -1,8 +1,33 @@
 use std::env;
 
+use rand::rng;
+use rand::prelude::SliceRandom;
+
+fn print_grid(grid:&Vec<bool>) {
+	for (index,value) in grid.iter().enumerate() {
+		//let (index,value) = e.enumerate();
+		println!("grid[{}]={}",index,value);
+	}
+}
+
 fn main() {
 	let args: Vec<String> = env::args().collect();
-	let n = args[1].parse::<i32>().unwrap();
+	dbg!(&args);
+	let n = args[1].parse::<usize>().unwrap();
 	dbg!(n);
-    dbg!(args);
+	let mut grid : Vec<bool> = vec![false; n*n];
+
+	let b = args[2].parse::<usize>().unwrap();
+	dbg!(b);
+
+	// init 
+	for k in 0..b {
+		grid[k]=true;
+	}
+
+	print_grid(&grid);
+	println!("** shuffle **");
+	let mut rng = rng();
+	grid.shuffle(&mut rng);
+	print_grid(&grid);
 }
