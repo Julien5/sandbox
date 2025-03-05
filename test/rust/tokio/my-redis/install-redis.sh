8cedb2c468031f2ac5bf5652b90728b4da3ccf0d
Julien5 <julien_bourgeois@yahoo.fr>
2025-03-05 09:37:50 +0100

    make hello tokio

--- test/rust/tokio/my-redis/install-redis.sh
+++ test/rust/tokio/my-redis/install-redis.sh
@@ -6,9 +6,12 @@ source ~/projects/config/profile/profile.sh
 dev.rust.desktop
 # install server
 # cargo install mini-redis
-echo start server
+echo test server
 killall mini-redis-server || true
 sleep 1
 mini-redis-server &
 sleep 1
 mini-redis-cli get foo
+killall mini-redis-server || true
+
+cargo new my-redis
