418fcb7bce5cd14565bcc0d13ff77912fdda3bfb
Julien5 <julien_bourgeois@yahoo.fr>
2025-03-09 16:49:18 +0100

    more playing with tasks

--- test/rust/tokio/my-redis/src/bin/tasks.rs
+++ test/rust/tokio/my-redis/src/bin/tasks.rs
@@ -1,31 +1,59 @@
 use tokio::time::Duration;
 
-struct Foo {
+struct Loader {
+	url: String,
+}
+
+struct Sink {
 	n: i32,
 }
 
-impl Foo {
-	fn new(n:i32) -> Foo {
-		Foo{n:n}
+impl Sink {
+	fn new(n:i32) -> Sink {
+		Sink{n:n}
 	}
-	async fn compute(&mut self) {
-		println!("Foo-{} compute starts",self.n);
-		tokio::time::sleep(Duration::from_secs(1)).await;
-		self.n=self.n+1;
-		println!("Foo-{} compute done",self.n);
+	fn print(self) {
+		println!("sink-{}",self.n);
 	}
 }
 
+async fn wait(n:i32) {
+	tokio::time::sleep(Duration::from_secs(n as u64)).await;
+}
+
+impl Loader {
+	fn new(url:String) -> Loader {
+		Loader{url:url}
+	}
+	async fn run(&mut self) -> Sink {
+		println!("Loader-{} step 1",self.url);
+		wait(1).await;
+		println!("Loader-{} step 2",self.url);
+		wait(1).await;
+		println!("Loader-{} step 3",self.url);
+		wait(1).await;
+		let s = Sink::new(1);
+		s
+	}
+}
+
+fn start_loader(s:String) -> tokio::task::JoinHandle<Sink> {
+	let mut foo = Loader::new(s.clone());
+	let handle = tokio::spawn(async move {
+		foo.run().await
+	});
+	handle
+}
+
+
 #[tokio::main]
 async fn main() {
-	let mut foo1 = Foo::new(0);
-    let task1 = tokio::spawn(async move {
-		foo1.compute().await;
-    });
-	let mut foo2 = Foo::new(2);
-	let task2 = tokio::spawn(async move {
-        foo2.compute().await;
-    });
-	task1.await.unwrap();
-	task2.await.unwrap();
+    let task1 = start_loader("fip".to_string());
+	let task2 = start_loader("france-musique".to_string());
+	wait(2).await;
+	task2.abort();
+	let res=task1.await.unwrap();
+	res.print();
+	//println!("task1 is finished {}",task1.is_finished());
+	//println!("task2 is finished {}",task2.is_finished());
 }
