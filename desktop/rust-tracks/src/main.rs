ad16ad3f26e2b497d5378317d169933e62c9f47a
Julien5 <julien_bourgeois@yahoo.fr>
2025-03-15 09:20:32 +0100

    try the distance with geoutils (still bad result)

--- desktop/rust-tracks/src/main.rs
+++ desktop/rust-tracks/src/main.rs
@@ -1,6 +1,7 @@
 extern crate gpx;
 extern crate chrono;
 extern crate geographiclib_rs;
+extern crate geoutils;
 
 use std::io::BufReader;
 use std::fs::File;
@@ -9,7 +10,6 @@ use gpx::read;
 use gpx::{Gpx, Track, TrackSegment};
 use geographiclib_rs::{Geodesic, InverseGeodesic};
 
-use chrono::format::ParseError;
 
 type Time = chrono::NaiveDateTime;
 
@@ -20,7 +20,7 @@ fn time_delta(t0:&Time,t1:&Time) -> i64 {
 
 fn kmh(speed:f64) -> f64 {
 	// m/s => kmh
-	speed * 3600f64 / 60f64
+	speed * 3.6f64 
 }
 
 fn main() {
@@ -31,20 +31,14 @@ fn main() {
     let track: &Track = &gpx.tracks[0];
     let segment: &TrackSegment = &track.segments[0];
 
-	// Determine the distance between two points - the "inverse" geodesic calculation.
-
-
 	let g = Geodesic::wgs84();
-	let p1 = (34.095925, -118.2884237);
-	let p2 = (59.4323439, 24.7341649);
-	let s12: f64 = g.inverse(p1.0, p1.1, p2.0, p2.1);
 
 	let mut previous: Option<(f64,f64,Time)> = None;
-
+	let mut total_distance = 0f64;
+	let mut total_time = 0i64;
 	for point in &segment.points {
 		let (x,y)=point.point().x_y();
 		let elevation=point.elevation.unwrap();
-		let speed=point.speed.unwrap_or_default();
 		if point.time.is_none() {
 			break;
 		}
@@ -52,11 +46,21 @@ fn main() {
 		let timestring=point.time.unwrap().format().unwrap();
 		let time : Time = chrono::NaiveDateTime::parse_from_str(timestring.as_str(),"%Y-%m-%dT%H:%M:%S.%fZ").ok().unwrap();
 		let (distance,dtime) = match previous {
-			Some((xprev,yprev,tprev)) => (g.inverse(x,y,xprev,yprev),time_delta(&tprev,&time)),
+			Some((xprev,yprev,tprev)) => {
+				let p0 = geoutils::Location::new(xprev, yprev);
+				let p1 = geoutils::Location::new(x, y);
+				let distance = p0.distance_to(&p1).unwrap().meters();
+				(distance,time_delta(&tprev,&time))},
 			None => (0f64,0i64),
 		};
-		let speed = distance/dtime as f64;
-		println!("point: ({x:.2},{y:.2}) {elevation:.0} {speed:.0} {distance:4.0}  {dtime:3.0} kmh");
+		total_distance += distance;
+		total_time += dtime;
+		let speed = kmh(distance/dtime as f64);
+		println!("point: ({x:.2},{y:.2}) {elevation:.0}m {distance:4.0}m {dtime:3.0}s {speed:.1} kmh");
 		previous = Some((x,y,time));
 	}
+	let total_distance_km = total_distance/1000f64;
+	let tt=chrono::TimeDelta::new(total_time,0).unwrap().num_minutes();
+	let avg=kmh(total_distance/total_time as f64);
+	println!("total: distance:{total_distance_km:.1} km time:{tt} min speed:{avg:.2} kmh");
 }
