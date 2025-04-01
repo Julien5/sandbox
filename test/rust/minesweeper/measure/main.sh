26eb14976e49bdacef565557fd05265c35c3951f
Julien5 <julien_bourgeois@yahoo.fr>
2025-04-01 07:07:04 +0200

    pass the exe as argument to measure

--- test/rust/minesweeper/measure/main.sh
+++ test/rust/minesweeper/measure/main.sh
@@ -4,9 +4,8 @@ set -e
 set -x
 
 function main() {
-	EXE=/home/julien/delme/rust-targets/debug/minesweeper
 	/usr/bin/time --output=/tmp/output.txt --format="%e s\n%M kB" \
-				  ${EXE} $@
+				  $@
 	cat /tmp/output.txt 
 }
 
