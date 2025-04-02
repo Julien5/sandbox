2d1a91c376388412a5210063dd1cd8804249650e
Julien5 <julien_bourgeois@yahoo.fr>
2025-04-02 18:02:08 +0200

    cleanup

--- test/rust/minesweeper/src/tile.rs
+++ test/rust/minesweeper/src/tile.rs
@@ -19,19 +19,19 @@ pub struct Tile {
 
 impl Tile {
 	fn index(&self, kn:usize, km:usize) -> usize {
-		_1d((kn+1,km+1),self.n+2,self.m+2)
+		_1d((kn+1,km+1),self.nM,self.mM)
 	}
 
 	fn index_all(&self, kn:usize, km:usize) -> usize {
-		_1d((kn,km),self.n+2,self.m+2)
+		_1d((kn,km),self.nM,self.mM)
 	}
 	
 	fn at(&self, kn:usize, km:usize) -> Element {
-		self.grid[_1d((kn+1,km+1),self.n+2,self.m+2)]
+		self.grid[_1d((kn+1,km+1),self.nM,self.mM)]
 	}
 
 	fn at_all(&self, kn:usize, km:usize) -> Element {
-		self.grid[_1d((kn,km),self.n+2,self.m+2)]
+		self.grid[_1d((kn,km),self.nM,self.mM)]
 	}
 	
 	pub fn with_chunk(chunk:BombChunk) -> Tile {
@@ -55,23 +55,31 @@ impl Tile {
 	}
 
 	pub fn update_first_real_line(&mut self, k:usize, prev: &Tile) {
-		if self.grid[self.nM+k] == BOMB {
+		let kg=self.index_all(k,1);
+		assert_eq!(kg,self.nM+k);
+		if self.grid[kg] == BOMB {
 			return;
 		}
-		let kg=self.index_all(k,1);
 		let kp=prev.index_all(k,self.mM-1);
 		self.grid[kg] += prev.grid[kp]; 
 	}
 
+	pub fn update_bottom_real_line(&mut self, k:usize, next: &Tile) {
+		let kg=self.index_all(k,self.mM-2);
+		assert_eq!(kg,(self.mM-2)*self.nM+k);
+		if self.grid[kg] == BOMB {
+			return;
+		}
+		let kn=next.index_all(k,0);
+		assert_eq!(kn,k);
+		self.grid[kg] += next.grid[kn];
+	}
+
 	pub fn merge(&mut self,prev:Option<&Tile>, next:Option<&Tile>) {
-		let m=self.bomb_chunk.m();
-		let n=self.bomb_chunk.n();
-		let mM=m+2;
-		let nM=n+2;
 		match prev {
 			Some(prev) => {
 				// update first "real" line
-				for k in 0..nM {
+				for k in 0..self.nM {
 					self.update_first_real_line(k,prev);
 				}
 			}
@@ -80,10 +88,8 @@ impl Tile {
 		match next {
 			Some(next) => {
 				// update last "real" line
-				for k in 0..nM {
-					if self.grid[(mM-2)*nM+k] != BOMB {
-						self.grid[(mM-2)*nM+k] += next.grid[k];
-					}
+				for k in 0..self.nM {
+					self.update_bottom_real_line(k,next);
 				}
 			}
 			None => {}
@@ -93,16 +99,12 @@ impl Tile {
 	pub fn print(&self, printer: &mut Printer) {
 		let print_lookup: [u8;11] = [b' ',b'1',b'2',b'3',b'4',b'5',b'6',b'7',b'8',b'B',b' '];
 		assert!(!self.grid.is_empty());
-		let n=self.bomb_chunk.n();
-		let m=self.bomb_chunk.m();
-		let nM=n+2;
-		let mM=m+2;
-		let mut output:Vec<u8> = vec![b' ';n+2];
-		output[n]=b'|';
-		output[n+1]=b'\n';
-		for km in 0..m {
-			for kn in 0..n {
-				let k=_1d((kn+1,km+1),nM,mM);
+		let mut output:Vec<u8> = vec![b' ';self.n+2];
+		output[self.n]=b'|';
+		output[self.n+1]=b'\n';
+		for km in 0..self.m {
+			for kn in 0..self.n {
+				let k=_1d((kn+1,km+1),self.nM,self.mM);
 				output[kn]=print_lookup[self.grid[k]];
 			}
 			printer.print(&output);
@@ -112,15 +114,11 @@ impl Tile {
 	pub fn _print_all(&self, printer: &mut Printer) {
 		let print_lookup: [u8;11] = [b' ',b'1',b'2',b'3',b'4',b'5',b'6',b'7',b'8',b'B',b' '];
 		assert!(!self.grid.is_empty());
-		let n=self.bomb_chunk.n();
-		let m=self.bomb_chunk.m();
-		let nM=n+2;
-		let mM=m+2;
-		let mut output:Vec<u8> = vec![b' ';nM+1];
-		output[nM]=b'\n';
-		for km in 0..mM {
-			for kn in 0..nM {
-				let k=_1d((kn,km),nM,mM);
+		let mut output:Vec<u8> = vec![b' ';self.nM+1];
+		output[self.nM]=b'\n';
+		for km in 0..self.mM {
+			for kn in 0..self.nM {
+				let k=_1d((kn,km),self.nM,self.mM);
 				output[kn]=print_lookup[self.grid[k]];
 			}
 			printer.print(&output);
@@ -128,17 +126,15 @@ impl Tile {
 	}
 
 	fn increment_neighboors(&mut self, pos:usize) {
-		let n=self.bomb_chunk.n();
-		let m=self.bomb_chunk.m();
-		let (posx,posy)=_2d(pos,n+2,m+2);
-		assert!(posx>0 && posx<n+1);
-		assert!(posy>0 && posy<m+1);
+		let (posx,posy)=_2d(pos,self.nM,self.mM);
+		assert!(posx>0 && posx<self.n+1);
+		assert!(posy>0 && posy<self.m+1);
 		for i in 0..3 {
 			for j in 0..3 {
 				if i == 1 && j == 1 {
 					continue
 				}
-				let l=_1d((posx+i-1,posy+j-1),n+2,m+2);
+				let l=_1d((posx+i-1,posy+j-1),self.nM,self.mM);
 				assert!(l<self.grid.len());
 				if self.grid[l] != BOMB {
 					self.grid[l]+=1;
