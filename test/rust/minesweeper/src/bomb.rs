dd2bc942e357c8fbf50c50f0350a1cd6f48a8b07
Julien5 <julien_bourgeois@yahoo.fr>
2025-04-04 16:55:05 +0200

    Fisher-Yates shuffle in rust

--- test/rust/minesweeper/src/bomb.rs
+++ test/rust/minesweeper/src/bomb.rs
@@ -1,6 +1,5 @@
 use rand::rngs::StdRng;
-use rand::prelude::SliceRandom;
-use rand::SeedableRng;
+use rand::{Rng, SeedableRng};
 
 use crate::utils::*;
 type BombPositions = Vec<usize>;
@@ -13,26 +12,24 @@ pub struct BombChunk {
 	positions:BombPositions
 }
 
-pub struct BombChunks {
-	chunks : Vec<BombChunk>,
-}
-
 fn distinct_random_numbers(n:usize,m:usize,b:usize) -> BombPositions {
-	// generates [0,1,...,N-1]
-	let mut G : Vec<usize>=(0usize..(n*m)).collect();
-	//let mut rng = StdRng::from_os_rng();
 	let mut rng = StdRng::seed_from_u64(3);
-	// shuffle it and keep the first b elements.
-	G.shuffle(&mut rng);
-	G.truncate(b);
-	assert_eq!(G.len(),b);
-	for g in &mut G {
-		let (x,y)=_2d(*g,n,m);
+	let mut positions : Vec<usize>=(0usize..(n*m)).collect();
+	for i in 0..b {
+		let end=m*n-i;
+		let j=rng.random_range(0..end);
+		positions.swap(j,end-1);
+	}
+	let mut G : Vec<usize>=vec![0;b];
+	let end=m*n;
+	for i in 0..b  {
+		let g = positions[end-i-1];
+		let (x,y)=_2d(g,n,m);
 		let p2=_1d((x+1,y+1),n+2,m+2);
 		let (px,py)=_2d(p2,n+2,m+2);
 		assert!(px>0 && px<(n+2-1));
 		assert!(py>0 && py<(m+2-1));
-		*g=p2
+		G[i]=p2;
 	}
 	G
 }
@@ -59,29 +56,5 @@ impl BombChunk {
 	pub fn index(&self) -> usize {
 		self.index
 	}
-}
-
-impl BombChunks {
-	pub fn new() -> BombChunks {
-		let ret=BombChunks {
-			chunks:vec![]
-		};
-		ret
-	}
-	pub fn push(&mut self, chunk:BombChunk) {
-		self.chunks.push(chunk);
-	}
-}
-
-impl Iterator for BombChunks {
-    type Item = BombChunk;
-    fn next(&mut self) -> Option<Self::Item> {
-		if self.chunks.is_empty() {
-			return None;
-		}
-		let N=self.chunks.len();
-		//println!("remaining number of bomb chunks:{N}");
-        Some(self.chunks.remove(N-1))
-    }
 }
 
