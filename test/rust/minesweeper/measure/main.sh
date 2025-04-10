75643a4db99e6c12ddba4f6cac8b769cfcb724d0
Julien5 <julien_bourgeois@yahoo.fr>
2025-04-10 07:17:36 +0200

    prints "TIMEs SPACEk" output

--- test/rust/minesweeper/measure/main.sh
+++ test/rust/minesweeper/measure/main.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 
 set -e
-set -x
+# set -x
 
+function measure() {
+	TMPFILE2=$(mktemp /tmp/measure.XXX.txt)
+	/usr/bin/time --output=${TMPFILE2} --format="%e s\n%M kB" \
+				  $@ > /tmp/devnull
+	cat ${TMPFILE2} | tr "\n" " "
+	rm -f ${TMPFILE2}
+}
 function main() {
-	/usr/bin/time --output=/tmp/output.txt --format="%e s\n%M kB" \
-				  $@ > /dev/null
-	cat /tmp/output.txt 
+	TMPFILE=$(mktemp /tmp/measure.XXX.txt)
+	measure "$@" &> ${TMPFILE}
+	TIME=$(tail -1 ${TMPFILE} | cut -f1 -d" ")
+	SPACE=$(tail -1 ${TMPFILE} | cut -f3 -d" ")
+	printf "%ss %sk\n" "${TIME}" "${SPACE}"
+	rm -f ${TMPFILE1}
 }
 
 main "$@"
