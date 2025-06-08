cb3a69a68262557ae64013693d7c31f62be1239f
Julien5 <julien_bourgeois@yahoo.fr>
2025-06-08 09:24:29 +0200

    parse and apply transform

--- desktop/track/profile/frontend/minisvg/lib/src/minisvg.dart
+++ desktop/track/profile/frontend/minisvg/lib/src/minisvg.dart
@@ -2,27 +2,67 @@ import 'dart:developer' as developer;
 import 'dart:io';
 import 'dart:ui';
 
-import 'package:flutter/widgets.dart';
+import 'package:flutter/foundation.dart';
 import 'package:xml/xml.dart';
 
 class Transform {
-  double sx = 1.0;
-  double sy = 1.0;
-  double tx = 0.0;
-  double ty = 0.0;
+  static List<Transform> readAttribute(String s) {
+    List<Transform> transforms = [];
+    final transformRegex = RegExp(r'(translate\([^)]+\)|scale\([^)]+\))');
+
+    // Match all transformations in order
+    for (final match in transformRegex.allMatches(s)) {
+      final transform = match.group(0)!;
+      if (transform.startsWith('translate')) {
+        transforms.add(Translate(transform));
+      } else if (transform.startsWith('scale')) {
+        transforms.add(Scale(transform));
+      }
+    }
+
+    return transforms;
+  }
 }
 
-class Node {
-  late Transform T;
+class Scale extends Transform {
+  double sx = 1.0;
+  double sy = 1.0;
+  Scale(String s) {
+    developer.log(s);
+    final scaleRegex = RegExp(r'scale\(([^,\s]+)[,\s]+([^)]+)\)');
+    final scaleMatch = scaleRegex.firstMatch(s);
+    assert(scaleMatch != null);
+    sx = double.parse(scaleMatch!.group(1)!);
+    sy = double.parse(scaleMatch.group(2)!);
+  }
+}
+
+class Translate extends Transform {
+  double tx = 0.0;
+  double ty = 0.0;
+  Translate(String s) {
+    developer.log(s);
+    final translateRegex = RegExp(r'translate\(([^,\s]+)[,\s]+([^)]+)\)');
+    final translateMatch = translateRegex.firstMatch(s);
+    assert(translateMatch != null);
+    tx = double.parse(translateMatch!.group(1)!);
+    ty = double.parse(translateMatch  .group(2)!);
+  }
+}
+
+abstract class Element {
+  List<Transform> T = [];
   List<Element> children = [];
 
+  void paintElement(Canvas canvas, Size size);
+
   late XmlElement e;
-  Node(XmlElement pe) : e = pe {
-    T = Transform(); //parse transform attributes if any
+  Element(XmlElement pe) : e = pe {
     if (e.attributes.isNotEmpty) {
       for (var attr in e.attributes) {
         switch (attr.name.local) {
           case 'transform':
+            T = Transform.readAttribute(attr.value);
             break;
           default:
             // Handle other attributes if necessary
@@ -31,6 +71,26 @@ class Node {
       }
     }
   }
+
+  void installTransforms(Canvas canvas) {
+    canvas.save();
+    for (var t in T) {
+      if (t is Translate) {
+        developer.log('install translate (${t.tx},${t.ty})');
+        canvas.translate(t.tx, t.ty);
+      }
+      if (t is Scale) {
+        developer.log('install scale (${t.sx},${t.sy})');
+        canvas.scale(t.sx, t.sy);
+      }
+    }
+  }
+
+  void deinstallTransforms(Canvas canvas) {
+    developer.log('desinstall transforms');
+    canvas.restore();
+  }
+
   static Element fromXml(XmlElement e) {
     if (e.name.local == "path") {
       return PathElement(e);
@@ -46,28 +106,23 @@ class Node {
   }
 }
 
-abstract class Element extends Node {
-  Element(super.e);
-  void paintElement(Canvas canvas, Size size);
-}
-
 class GroupElement extends Element {
   GroupElement(super.e) {
-    // set super.T;
     for (var child in e.children) {
       if (child is XmlElement) {
-        children.add(Node.fromXml(child));
+        children.add(Element.fromXml(child));
       }
     }
-    // unset super.T;
   }
 
   @override
   void paintElement(Canvas canvas, Size size) {
     developer.log('group paint ${super.e.name.local}');
+    installTransforms(canvas);
     for (var child in children) {
       child.paintElement(canvas, size);
     }
+    deinstallTransforms(canvas);
   }
 }
 
@@ -76,7 +131,9 @@ class PathElement extends Element {
 
   @override
   void paintElement(Canvas canvas, Size size) {
+    installTransforms(canvas);
     developer.log('path paint ${super.e.name.local}');
+    deinstallTransforms(canvas);
   }
 }
 
@@ -85,15 +142,17 @@ class TextElement extends Element {
 
   @override
   void paintElement(Canvas canvas, Size size) {
+    installTransforms(canvas);
     developer.log('text paint ${super.e.name.local}');
+    deinstallTransforms(canvas);
   }
 }
 
 String mini() {
   /// read xml from file
-  String xml = File('track-0.svg').readAsStringSync();
+  String xml = File('test.svg').readAsStringSync();
   XmlDocument doc = XmlDocument.parse(xml);
-  Element root = Node.fromXml(doc.rootElement);
+  Element root = Element.fromXml(doc.rootElement);
   Canvas canvas = Canvas(PictureRecorder());
   Size size = Size(100, 100); // Example size, adjust as needed
   root.paintElement(canvas, size);
