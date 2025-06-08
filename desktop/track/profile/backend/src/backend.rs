714cf5b9b1267b7fbf813a0194022850e878b783
Julien5 <julien_bourgeois@yahoo.fr>
2025-06-08 07:38:50 +0200

    save the rendered svg in a tmp file (track)

--- desktop/track/profile/backend/src/backend.rs
+++ desktop/track/profile/backend/src/backend.rs
@@ -126,7 +126,10 @@ impl Backend {
         let range = &segment.range;
         self.enrichWaypoints();
         let bbox = ProfileBoundingBox::from_track(&self.track, &range);
-        render::track_profile(&self.track, &range, &bbox)
+        let ret = render::track_profile(&self.track, &range, &bbox);
+        let filename = std::format!("/tmp/track-{}.svg", segment.id);
+        std::fs::write(filename, &ret).expect("Unable to write file");
+        ret
     }
     pub fn render_segment_waypoints(&mut self, segment: &Segment) -> String {
         println!("render_segment_waypoints:{}", segment.id);
