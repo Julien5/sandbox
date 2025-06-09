26c022417622a5efdc9034a57ef53ca0678d2a04
Julien5 <julien_bourgeois@yahoo.fr>
2025-06-09 10:32:06 +0200

    integrate minisvg into ui

--- desktop/track/profile/frontend/ui/lib/src/future_rendering_widget.dart
+++ desktop/track/profile/frontend/ui/lib/src/future_rendering_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:ui/src/backendmodel.dart';
+import 'package:ui/src/minisvg.dart';
 
 class FutureRenderingWidget extends StatefulWidget {
   final FutureRenderer future;
@@ -25,7 +26,8 @@ class _FutureRenderingWidgetState extends State<FutureRenderingWidget> {
   @override
   Widget build(BuildContext context) {
     if (widget.future.done()) {
-      svg = SvgPicture.string(widget.future.result(), width: 1400, height: 400);
+      //svg = SvgPicture.string(widget.future.result(), width: 1400, height: 400);
+      svg=MiniSvgWidget(svg: widget.future.result(), width: 1400, height: 400);
     }
     if (!widget.future.done() && svg == null) {
       return Text("starting ${widget.future.trackData} ${widget.future.id()}");
