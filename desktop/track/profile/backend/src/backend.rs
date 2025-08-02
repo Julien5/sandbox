16fdf4a6558e5a56d614cbb27dc270ca43f8d564
Julien5 <julien_bourgeois@yahoo.fr>
2025-08-02 18:57:54 +0200

    minor minor minor

--- desktop/track/profile/backend/src/backend.rs
+++ desktop/track/profile/backend/src/backend.rs
@@ -278,7 +278,7 @@ impl Backend {
         profile.add_track(&self.track, &self.track_smooth_elevation);
         let ret = profile.render();
         let filename = std::format!("/tmp/segment-{}.svg", segment.id);
-        std::fs::write(filename, &ret).expect("Unable to write file");
+        //std::fs::write(filename, &ret).expect("Unable to write file");
         ret
     }
     pub fn render_segment_waypoints(
