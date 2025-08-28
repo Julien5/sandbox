596b5414f5ed285472d504260ba04938ebf76bbb
Julien5 <julien_bourgeois@yahoo.fr>
2025-08-28 19:38:08 +0200

    fix update of the graph

--- desktop/track/profile/backend/src/label_graph.rs
+++ desktop/track/profile/backend/src/label_graph.rs
@@ -20,28 +20,37 @@ impl Graph {
         }
     }
     fn intersect(&self, a: &Node, b: &Node) -> bool {
-        for ka in self.candidates.get(a).unwrap() {
-            for kb in self.candidates.get(b).unwrap() {
-                if ka.bbox.intersect(&kb.bbox) {
+        for ca in self.candidates.get(a).unwrap() {
+            for cb in self.candidates.get(b).unwrap() {
+                if ca.bbox.intersect(&cb.bbox) {
                     return true;
                 }
             }
         }
         false
     }
-    pub fn add_node(&mut self, a: Node, candidates: Candidates) {
-        debug_assert!(!self.map.contains_key(&a));
-        self.candidates.insert(a, candidates);
+    fn compute_edges(&mut self, a: &Node) {
         let mut E = Edges::new();
-        for b in self.map.keys().clone() {
+        for b in self.candidates.keys().clone() {
+            if b == a {
+                continue;
+            }
             if self.intersect(&a, b) {
                 E.push(*b);
             }
         }
-        for b in &E {
-            self.map.get_mut(b).unwrap().push(a);
+        self.map.insert(*a, E);
+    }
+    pub fn refresh_graph_from_candidates(&mut self) {
+        self.map.clear();
+        let nodes: Vec<_> = self.candidates.keys().cloned().collect();
+        for node in nodes {
+            self.compute_edges(&node);
         }
-        self.map.insert(a, E);
+    }
+    pub fn add_node(&mut self, a: Node, candidates: Candidates) {
+        debug_assert!(!self.map.contains_key(&a));
+        self.candidates.insert(a, candidates);
     }
     pub fn select(&mut self, a: &Node, selected: &Candidate) {
         // for all b connected to a
@@ -58,13 +67,10 @@ impl Graph {
             // selected a candidate
             let Cb = self.candidates.get_mut(&b).unwrap();
             Cb.retain(|cb| !selected.bbox.intersect(&cb.bbox));
-            // remove the (b,a) edge
-            let Eb = self.map.get_mut(&b).unwrap();
-            Eb.retain(|&x| x != *a);
         }
         // remove a
-        self.map.remove(a);
         self.candidates.remove(a);
+        self.refresh_graph_from_candidates();
 
         // this is not enough:
         // we must remove b-c edges if there is no candidate backing the edge.
@@ -90,12 +96,12 @@ impl Graph {
                 .map(|x| x.to_string())
                 .collect::<Vec<_>>()
                 .join(", ");
-            println!(
-                "node: {:1} edges:{:5} |C|={}",
-                node,
-                list,
-                self.candidates.get(node).unwrap().len()
-            );
+            match self.candidates.get(node) {
+                Some(candidates) => {
+                    println!("node: {:1} edges:{:5} |C|={}", node, list, candidates.len())
+                }
+                None => println!("node: {:1} edges:{:5} |C|=0", node, list),
+            };
         }
     }
 }
@@ -125,11 +131,13 @@ mod tests {
         let mut CD = Candidates::new();
         let ca1 = make_candidate(0, 0, 2, 2);
         let ca2 = make_candidate(2, 2, 3, 2);
-        let cb = make_candidate(4, 2, 3, 2);
-        assert!(ca2.bbox.intersect(&cb.bbox));
+        let cb1 = make_candidate(1, 0, 3, 2);
+        let cb2 = make_candidate(4, 2, 3, 2);
+        assert!(ca2.bbox.intersect(&cb2.bbox));
         CA.push(ca1);
         CA.push(ca2);
-        CB.push(cb.clone());
+        CB.push(cb1.clone());
+        CB.push(cb2.clone());
         let cc1 = make_candidate(3, 3, 2, 3);
         CC.push(cc1.clone());
         let cc2 = make_candidate(4, 3, 2, 3);
@@ -140,19 +148,19 @@ mod tests {
         graph.add_node(1, CB);
         graph.add_node(2, CC);
         graph.add_node(3, CD);
+        graph.refresh_graph_from_candidates();
 
         graph.print();
         assert!(graph.max_node() == 2);
         println!("select {} {}", 1, "cc1");
         graph.select(&2, &cc1);
-        assert!(!graph.map.contains_key(&2));
         graph.print();
+        assert!(!graph.map.contains_key(&2));
         assert!(graph.candidates.get(&0).unwrap().len() == 1);
-        assert!(graph.candidates.get(&1).unwrap().len() == 0);
-        // 0 and 1 should be disconnect since they have no overlaping
-        // candidates
-        assert!(graph.map.get(&0).unwrap().len() == 0);
-        assert!(graph.map.get(&1).unwrap().len() == 0);
+        assert!(graph.candidates.get(&1).unwrap().len() == 1);
+        assert!(graph.candidates.get(&3).unwrap().len() == 1);
+        assert!(graph.map.get(&0).unwrap().len() == 1);
+        assert!(graph.map.get(&1).unwrap().len() == 1);
         println!("max node {}", graph.max_node());
     }
 }
